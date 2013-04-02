8f0a6b73d08e7678f7ab1d6881ef6be9f9da3cff
Andrew Coomes <ajc@ajcdrift.com>
2013-04-02 16:55:17 -0700

    Added solution to problem 5.
    Signed-off-by: Andrew Coomes <ajc@ajcdrift.com>

--- problem5.pl
+++ problem5.pl
@@ -3,3 +3,19 @@
 
 # What is the smallest positive number that is evenly divisible by all 
 # of the numbers from 1 to 20?
+
+my $i;
+my $found = 0;
+
+while ( !$found ) {
+    $i += 2520;
+    $found = 1;
+    for ( my $n = 19; $n >= 11 ; $n-- ) {
+        if ( $i % $n ) {
+            $found = 0;
+            last;
+        }
+    }
+}
+
+print $i;
