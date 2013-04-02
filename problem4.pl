850ab18e5ab2cfb85a5f5ad61e3fd8a4399c68e9
Andrew Coomes <ajc@ajcdrift.com>
2013-04-02 13:21:21 -0700

    Added palindrome check subroutine.
    Signed-off-by: Andrew Coomes <ajc@ajcdrift.com>

--- problem4.pl
+++ problem4.pl
@@ -3,3 +3,16 @@
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
+# Palindrome Checker
+# Splits a scalar into an array, reverses it and joins it back into a scalar.
+sub palindromeChecker {
+    my $stringToCheck = shift;
+    my $isPalindrome = 0;
+    my @splitArray = split( "", $stringToCheck );
+    my @reversedArray = reverse( @splitArray );
+    my $reversedString = join( "", @reversedArray );
+    if ( $reversedString eq $stringToCheck ) {
+        $isPalindrome = 1;
+    }
+    return $isPalindrome;
+}
