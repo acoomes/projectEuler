ca93c626f9e8c907363299ccb38a25a5636a94d7
Andrew Coomes <ajc@ajcdrift.com>
2013-04-02 14:19:33 -0700

    Added solution to problem 4. Removed palindrome checker in favor of Perl's reverse function.
    Signed-off-by: Andrew Coomes <ajc@ajcdrift.com>

--- problem4.pl
+++ problem4.pl
@@ -3,16 +3,21 @@
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-# Palindrome Checker
-# Splits a scalar into an array, reverses it and joins it back into a scalar.
-sub palindromeChecker {
-    my $stringToCheck = shift;
-    my $isPalindrome = 0;
-    my @splitArray = split( "", $stringToCheck );
-    my @reversedArray = reverse( @splitArray );
-    my $reversedString = join( "", @reversedArray );
-    if ( $reversedString eq $stringToCheck ) {
-        $isPalindrome = 1;
+my $a;
+my $b;
+my $product;
+my $largestPalindrome;
+
+# Loop $a and $b down to 100.
+for ( $a = 999; $a > 99; $a-- ) {
+    for ( $b = 999; $b > 99; $b-- ) {
+        $product = $a * $b;
+        # If $product is a palindrome, check to see whether it is larger than 
+        # $largestPalindrome. If true, set it as $largestPalindrome.
+        if ( $product eq reverse $product and $product > $largestPalindrome ) {
+            $largestPalindrome = $product;
+        }
     }
-    return $isPalindrome;
 }
+
+print "\n  $largestPalindrome is the largest palindrome.\n";
