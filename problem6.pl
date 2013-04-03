daa41a0f088c24416a88a308ec3717512899ea91
Andrew Coomes <ajc@ajcdrift.com>
2013-04-03 10:03:11 -0700

    Added solution to problem 6.
    Signed-off-by: Andrew Coomes <ajc@ajcdrift.com>

--- problem6.pl
+++ problem6.pl
@@ -7,7 +7,24 @@
 # (1 + 2 + ... + 10)^2 = 552 = 3025
 
 # Hence the difference between the sum of the squares of the first 
-# ten natural numbers and the square of the sum is 3025  385 = 2640.
+# ten natural numbers and the square of the sum is 3025 - 385 = 2640.
 
 # Find the difference between the sum of the squares of the first 
 # one hundred natural numbers and the square of the sum.
+
+my $sumOfSquares;
+my $squareOfSums;
+my $difference;
+
+for ( my $i = 100; $i > 0; $i-- ) {
+    $sumOfSquares += $i * $i;
+}
+
+for ( my $i = 100; $i > 0; $i-- ) {
+    $squareOfSums += $i;
+}
+$squareOfSums = $squareOfSums * $squareOfSums;
+
+$difference = $squareOfSums - $sumOfSquares;
+
+print "\n  $difference\n";
