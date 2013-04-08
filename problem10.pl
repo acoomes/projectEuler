bb5d58b1f0ec570602d9804ff6ba9bcc514119a6
Andrew Coomes <ajc@ajcdrift.com>
2013-04-08 13:48:32 -0700

    Added primeChecker function.
    Signed-off-by: Andrew Coomes <ajc@ajcdrift.com>

--- problem10.pl
+++ problem10.pl
@@ -1,3 +1,20 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 # Find the sum of all the primes below two million.
+
+# Checks whether a given number is prime.
+sub primeChecker {
+    my $numToCheck = shift;
+    my $isPrime = 1;
+
+    # Loop from 1 to the square root of $numToCheck.
+    # If $numToCheck is evenly divisible by an 
+    # iteration return false.
+    for ( my $i = 2; $i <= sqrt $numToCheck; $i++ ) {
+        if ( $numToCheck % $i eq 0 ) {
+            $isPrime = 0;
+            last;
+        }
+    }
+    return $isPrime;
+}
