48f059345c3cf2c241b86c57fcde909a1c37b8e2
Andrew Coomes <ajc@ajcdrift.com>
2013-04-08 16:47:19 -0700

    Added solution to problem 10.
    Signed-off-by: Andrew Coomes <ajc@ajcdrift.com>

--- problem10.pl
+++ problem10.pl
@@ -2,12 +2,24 @@
 
 # Find the sum of all the primes below two million.
 
+my $i = 2;
+my $sum;
+
+while( $i < 2000000 ) {
+    if ( primeChecker( $i ) ) {
+        $sum += $i;
+    }
+    $i++;
+}
+
+print "\n  $sum\n";
+
 # Checks whether a given number is prime.
 sub primeChecker {
     my $numToCheck = shift;
     my $isPrime = 1;
 
-    # Loop from 1 to the square root of $numToCheck.
+    # Loop from 2 to the square root of $numToCheck.
     # If $numToCheck is evenly divisible by an 
     # iteration return false.
     for ( my $i = 2; $i <= sqrt $numToCheck; $i++ ) {
